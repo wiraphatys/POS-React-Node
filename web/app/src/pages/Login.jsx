d3629d375dada2ac5cc6dcefc55b6f4872c107c6
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2023-12-22 20:26:34 +0700

    create login page

---
+++ web/app/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+        <div className="text-center h3">
+            Login to POS Service
+        </div>
+    </div>
+  )
+}
+
+export default Login
